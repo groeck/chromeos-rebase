3a57828464f4c99d1a9b6d21f83099d7458445ee
Guenter Roeck <groeck@chromium.org>
2017-06-27 08:58:41 -0700

    Update config file for v4.12
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
-patchlist = "patchlist-3584f61144fd"
+patchlist = "patchlist-170626"
 
-rebasedb = 'rebase49.db'
-upstreamdb = 'upstream49.db'
+rebasedb = 'rebase412.db'
+upstreamdb = 'upstream412.db'
+nextdb = 'next412.db'
 
+rebase_origin = 'chromeos-4.4'
 rebase_baseline = 'v4.4'
-stable_baseline = 'v4.4.52'
-rebase_target = 'v4.9'
+stable_baseline = 'v4.4.70'
+rebase_target = 'v4.12-rc7'
 
 chromeos_path = "linux-chrome"
 stable_path = "linux-stable"
 android_path = "linux-android"
 upstream_path = "linux-upstream"
+next_path = "linux-next"
 
 droplist = [ ("drivers/net/wireless/iwl7000", "intel"),
 	( "include/config/iwl7000", "intel" ),
@@ -23,7 +26,6 @@ droplist = [ ("drivers/net/wireless/iwl7000", "intel"),
 	( "Documentation/devicetree/bindings/media/mediatek", "mediatek" ),
 	( "Documentation/devicetree/bindings/soc/mediatek", "mediatek" ),
 	( "sound/soc/mediatek", "mediatek" ),
-	( "sound/soc/intel", "intel" ),
 	( "drivers/cpufreq/mt8173", "mediatek" ),
 	( "arch/arm64/boot/dts/mediatek", "mediatek" ),
 	( "drivers/net/wireless/ar10k", "atheros" ),
@@ -37,6 +39,10 @@ topiclist = [ [ "chromeos", "COMMIT-QUEUE.ini", "PRESUBMIT.cfg"],
 	  "drivers/pwm/pwm-cros-ec.c", "drivers/iio/common/cros_ec_sensors", "drivers/regulator/cros_ec",
 	  "drivers/i2c/busses/i2c-cros-ec", "include/linux/mfd/cros_ec", "include/linux/chromeos",
 	  "Documentation/devicetree/bindings/chrome" ],
+	[ "drivers/gpu/drm/bridge/analogix", "include/drm/bridge/analogix_dp.h",
+	  "Documentation/devicetree/bindings/display/bridge/analogix_dp.txt",
+	  "Documentation/devicetree/bindings/drm/bridge/anx7688.txt" ],
+	[ "drivers/gpu/arm/midgard", "Documentation/devicetree/bindings/arm/mali-midgard.txt" ],
 	[ "arch/arm64/boot/dts/rockchip", "arch/arm/boot/dts/rockchip", "arch/arm/boot/dts/rk3" ],
 	[ "drivers/devfreq/rk3399", "drivers/devfreq/event/rockchip" ],
 	[ "drivers/clk/rockchip", "include/dt-bindings/clock/rk3399" ],
@@ -44,10 +50,11 @@ topiclist = [ [ "chromeos", "COMMIT-QUEUE.ini", "PRESUBMIT.cfg"],
 	[ "drivers/phy/phy-rockchip", "Documentation/devicetree/bindings/phy/phy-rockchip" ],
 	[ "drivers/usb/dwc3/dwc3-rockchip" ],
 	[ "drivers/crypto/rockchip" ],
-	[ "drivers/gpu/drm/rockchip", "drivers/gpu/drm/bridge/analogix", "include/drm/bridge/analogix_dp.h",
-	  "Documentation/devicetree/bindings/display/rockchip", "drivers/media/platform/rockchip-vpu",
-	  "drivers/media/platform/rk3288", "Documentation/devicetree/bindings/display/rockchip/analogix_dp-rockchip.txt",
-	  "drivers/gpu/arm/midgard" ],
+	[ "drivers/gpu/drm/rockchip",
+	  "Documentation/devicetree/bindings/display/rockchip",
+	  "drivers/media/platform/rockchip-vpu",
+	  "drivers/media/platform/rk3288",
+	  "Documentation/devicetree/bindings/display/rockchip/analogix_dp-rockchip.txt" ],
 	[ "include/soc/rockchip", "sound/soc/rockchip", "drivers/soc/rockchip",
 	  "Documentation/devicetree/bindings/sound/rockchip", "arch/arm/mach-rockchip" ],
 	[ "include/dt-bindings/power/rk3399" ],
@@ -75,6 +82,7 @@ topiclist = [ [ "chromeos", "COMMIT-QUEUE.ini", "PRESUBMIT.cfg"],
 	[ "net/wireless", "drivers/net/wireless", "Documentation/devicetree/bindings/net/wireless" ],
 	[ "drivers/net/usb", "net", "drivers/net", "include/linux/tcp.h", "include/uapi/linux/tcp.h",
 	  "include/net", "include/dt-bindings/net", "include/linux/net", "include/uapi/linux/sockios.h" ],
+	[ "sound/soc/intel" ],
 	[ "sound", "Documentation/devicetree/bindings/sound", "include/sound", "include/uapi/sound" ],
 	[ "security", "include/linux/alt-syscall.h", "arch/arm64/kernel/alt-syscall.c",
 	  "arch/x86/kernel/alt-syscall.c", "kernel/alt-syscall.ch" ],
