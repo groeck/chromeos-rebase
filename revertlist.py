e2c0c87602f0751127d6c89208cd910ec7f2b777
Guenter Roeck <groeck@chromium.org>
2018-07-09 12:57:49 -0700

    revertlist.py: lintify
    Formatting changes only. Note that importing print_function
    from __future__ results in a syntax error in line 35.
    
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- revertlist.py
+++ revertlist.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-"
+# from __future__ import print_function
 import sqlite3
 import os
-import subprocess
 import re
 from config import rebasedb
 
@@ -26,32 +27,33 @@ rp = re.compile('Revert "(.*)"')
 # c.execute("INSERT INTO files VALUES (sha,filename)")
 # sha and filename must be in ' '
 
-# c.execute("CREATE TABLE commits (date integer, sha text, description text, sequence integer, disposition text, dsha text)")
-
 c.execute("select date, sha, description from commits")
 for (date, sha, desc) in c.fetchall():
-    m = rp.search(desc)
-    if m:
-        ndesc=m.group(1)
-	print "Found revert : '%s' (%s)" % (desc.replace("'", "''"), sha)
-        ndesc=ndesc.replace("'", "''")
-	c2.execute("select date, sha from commits where description is '%s'" % ndesc)
-	# Search for matching original commit. Only revert its first occurrance,
-	# and only if its date is older than the revert.
-	fsha=c2.fetchone()
-	if fsha:
-	    if date > fsha[0]:
-		# print "    Matching commit : %s" % fsha[1]
-		print "    Marking %s for drop" % fsha[1]
-		c2.execute("UPDATE commits SET disposition=('drop') where sha='%s'" % fsha[1])
-		c2.execute("UPDATE commits SET reason=('reverted') where sha='%s'" % fsha[1])
-	    else:
-	        print "    Matching commit later than reverse"
-	else:
-	    print "    No matching commit found"
-	print "    Marking %s for drop" % sha
-	c2.execute("UPDATE commits SET disposition=('drop') where sha='%s'" % sha)
-	c2.execute("UPDATE commits SET reason=('reverted') where sha='%s'" % sha)
+  m = rp.search(desc)
+  if m:
+    ndesc = m.group(1)
+    print "Found revert : '%s' (%s)" % (desc.replace("'", "''"), sha)
+    ndesc = ndesc.replace("'", "''")
+    c2.execute("select date, sha from commits where description is '%s'"
+               % ndesc)
+    # Search for matching original commit. Only revert its first occurrance,
+    # and only if its date is older than the revert.
+    fsha = c2.fetchone()
+    if fsha:
+      if date > fsha[0]:
+        # print "    Matching commit : %s" % fsha[1]
+        print "    Marking %s for drop" % fsha[1]
+        c2.execute("UPDATE commits SET disposition=('drop') where sha='%s'"
+                   % fsha[1])
+        c2.execute("UPDATE commits SET reason=('reverted') where sha='%s'"
+                   % fsha[1])
+      else:
+        print "    Matching commit later than reverse"
+    else:
+      print "    No matching commit found"
+    print "    Marking %s for drop" % sha
+    c2.execute("UPDATE commits SET disposition=('drop') where sha='%s'" % sha)
+    c2.execute("UPDATE commits SET reason=('reverted') where sha='%s'" % sha)
 
 conn.commit()
 
