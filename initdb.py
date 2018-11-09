0bd1780e3d84cd9892549a4eaebabdc0fab9684e
Guenter Roeck <groeck@chromium.org>
2018-11-09 14:46:53 -0800

    initdb: Rework into functions, update database by default
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- initdb.py
+++ initdb.py
@@ -2,38 +2,60 @@
 import sqlite3
 import os
 import subprocess
+import time
 from config import rebasedb, \
         stable_path, android_path, chromeos_path, \
         rebase_baseline, stable_baseline, rebase_target
 
 stable_commits = rebase_baseline + '..' + stable_baseline
+baseline_commits = rebase_baseline + '..'
 
 workdir = os.getcwd()
 
-try:
-  os.remove(rebasedb)
-except OSError:
-  pass
+def NOW():
+  return int(time.time())
 
-conn = sqlite3.connect(rebasedb)
-# conn.text_factory = str
+def removedb():
+  '''
+  remove database if it exists
+  '''
 
-c = conn.cursor()
+  try:
+    os.remove(rebasedb)
+  except OSError:
+    pass
 
-# Create table
-c.execute("CREATE TABLE commits (date integer, sha text, patchid text, description text, \
-                                topic integer, disposition text, reason text, \
-                                sscore integer, pscore integer, dsha text)")
-c.execute("CREATE UNIQUE INDEX commit_date ON commits (date)")
-c.execute("CREATE INDEX commit_sha ON commits (sha)")
-c.execute("CREATE INDEX patch_id ON commits (patchid)")
+def createdb():
+  '''
+  remove and recreate database
+  '''
 
-c.execute("CREATE TABLE files (sha text, filename text)")
-c.execute("CREATE INDEX file_sha ON files (sha)")
-c.execute("CREATE INDEX file_name ON files (filename)")
+  removedb
 
-c.execute("CREATE TABLE stable (sha, origin)")
-c.execute("CREATE UNIQUE INDEX stable_sha ON stable (sha)")
+  conn = sqlite3.connect(rebasedb)
+  c = conn.cursor()
+
+  # Create table
+  c.execute("CREATE TABLE commits (date integer, \
+                                   created timestamp, updated timestamp, \
+                                   sha text, patchid text, \
+                                   description text, topic integer, \
+				   disposition text, reason text, \
+                                   sscore integer, pscore integer, dsha text)")
+  c.execute("CREATE UNIQUE INDEX commit_date ON commits (date)")
+  c.execute("CREATE INDEX commit_sha ON commits (sha)")
+  c.execute("CREATE INDEX patch_id ON commits (patchid)")
+
+  c.execute("CREATE TABLE files (sha text, filename text)")
+  c.execute("CREATE INDEX file_sha ON files (sha)")
+  c.execute("CREATE INDEX file_name ON files (filename)")
+
+  c.execute("CREATE TABLE stable (sha, origin)")
+  c.execute("CREATE UNIQUE INDEX stable_sha ON stable (sha)")
+
+  # Save (commit) the changes
+  conn.commit()
+  conn.close()
 
 # date:
 #         git show --format="%ct" -s ${sha}
@@ -47,110 +69,128 @@ c.execute("CREATE UNIQUE INDEX stable_sha ON stable (sha)")
 # c.execute("INSERT INTO files VALUES (sha,filename)")
 # sha and filename must be in ' '
 
-# Save (commit) the changes
-conn.commit()
+def update_stable(path, list, origin):
+  '''
+  Create list of SHAs from provided path and commit list.
+  Skip if entry (sha) is already in database.
+  '''
 
-os.chdir(stable_path)
-commits = subprocess.check_output(['git', 'log', '--oneline', '--reverse',
-                                   stable_commits])
-os.chdir(workdir)
+  conn = sqlite3.connect(rebasedb)
+  c = conn.cursor()
 
-# Create list of SHAs from stable release
-for commit in commits.splitlines():
-  if commit != "":
-    elem = commit.split(" ")[:1]
-    c.execute("INSERT INTO stable(sha, origin) VALUES (?, ?)",
-              (elem[0], "stable", ))
+  os.chdir(path)
+  commits = subprocess.check_output(['git', 'log', '--oneline', '--reverse',
+                                     list])
+  os.chdir(workdir)
 
-conn.commit()
+  for commit in commits.splitlines():
+    if commit != "":
+      elem = commit.split(" ")[:1]
+      sha = elem[0]
+      c.execute("select sha from stable where sha is '%s'" % sha)
+      found = c.fetchall()
+      if found == []:
+        c.execute("INSERT INTO stable(sha, origin) VALUES (?, ?)",
+                  (sha, origin, ))
 
-# Create list of SHAs from android baseline
-# Skip if entry is already in database.
-os.chdir(android_path)
-commits = subprocess.check_output(['git', 'log', '--oneline', '--reverse',
-                                   rebase_baseline + '..'])
-os.chdir(workdir)
+  conn.commit()
+  conn.close()
 
-for commit in commits.splitlines():
-  if commit != "":
-    elem = commit.split(" ")[:1]
-    sha = elem[0]
-    c.execute("select sha from stable where sha is '%s'" % sha)
-    found = c.fetchall()
-    if found == []:
-      c.execute("INSERT INTO stable(sha, origin) VALUES (?, ?)",
-                (sha, "android", ))
+def update_commits():
+  '''
+  Get complete list of commits from rebase baseline.
+  Assume that the baseline branch exists and has been checked out.
+  '''
 
-conn.commit()
+  conn = sqlite3.connect(rebasedb)
+  c = conn.cursor()
 
-# get complete list of commits from rebase baseline
-# Assume that the baseline branch exists and has been checked out.
+  os.chdir(chromeos_path)
+  commits = subprocess.check_output(['git', 'log', '--oneline', "--reverse",
+                                     rebase_baseline + '..'])
 
-os.chdir(chromeos_path)
-commits = subprocess.check_output(['git', 'log', '--oneline', "--reverse",
-                                   rebase_baseline + '..'])
+  prevdate = 0
+  mprevdate = 0
+  for commit in commits.splitlines():
+    if commit != "":
+      elem = commit.split(" ", 1)
+      sha = elem[0]
+      description = elem[1].rstrip('\n')
+      description = description.decode('latin-1') \
+                  if isinstance(description, str) else description
 
-prevdate = 0
-mprevdate = 0
-for commit in commits.splitlines():
-  if commit != "":
-    elem = commit.split(" ", 1)
-    sha = elem[0]
-    description = elem[1].rstrip('\n')
-    description = description.decode('latin-1') \
-                if isinstance(description, str) else description
+      ps = subprocess.Popen(['git', 'show', sha], stdout=subprocess.PIPE)
+      spid = subprocess.check_output(['git', 'patch-id'], stdin=ps.stdout)
+      patchid = spid.split(" ", 1)[0]
 
-    ps = subprocess.Popen(['git', 'show', sha], stdout=subprocess.PIPE)
-    spid = subprocess.check_output(['git', 'patch-id'], stdin=ps.stdout)
-    patchid = spid.split(" ", 1)[0]
+      sdate = subprocess.check_output(['git', 'show', '--format="%ct"',
+                                       '-s', sha])
+      sdate = sdate.rstrip('\n')
+      sdate = sdate.strip('"')
+      # Make sure date is unique and in ascending order.
+      date = int(sdate)
+      if date == prevdate:
+        date = mprevdate + 1
+      else:
+        prevdate = date
+        date = date * 1000
+      mprevdate = date
 
-    sdate = subprocess.check_output(['git', 'show', '--format="%ct"',
-                                     '-s', sha])
-    sdate = sdate.rstrip('\n')
-    sdate = sdate.strip('"')
-    # Make sure date is unique and in ascending order.
-    date = int(sdate)
-    if date == prevdate:
-      date = mprevdate + 1
-    else:
-      prevdate = date
-      date = date * 1000
-    mprevdate = date
-    # Initially assume we'll drop everything because it is not listed when
-    # running "rebase -i".
-    c.execute("INSERT INTO commits(date, sha, patchid, description, disposition, reason) VALUES (?, ?, ?, ?, ?, ?)",
-              (date, sha, patchid, description, "drop", "merge",))
-    filenames = subprocess.check_output(['git', 'show', '--name-only',
-                                         '--format=', sha])
-    for fn in filenames.splitlines():
-      if fn != "":
-        c.execute("INSERT INTO files(sha, filename) VALUES (?, ?)", (sha, fn,))
+      # Do nothing if the sha is already in the commit table.
+      c.execute("select sha from commits where sha='%s'" % sha)
+      found = c.fetchone()
+      if found:
+        continue
 
-conn.commit()
+      # Initially assume we'll drop everything because it is not listed when
+      # running "rebase -i".
+      c.execute("INSERT INTO commits(date, created, updated, sha, patchid, description, disposition, reason) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                (date, NOW(), NOW(), sha, patchid, description, "drop", "default",))
+      filenames = subprocess.check_output(['git', 'show', '--name-only',
+                                           '--format=', sha])
+      for fn in filenames.splitlines():
+        if fn != "":
+          c.execute("INSERT INTO files(sha, filename) VALUES (?, ?)", (sha, fn,))
 
-# "git cherry -v <target>" on branch rebase_baseline gives us a list
-# of patches to apply.
-patches = subprocess.check_output(['git', 'cherry', '-v', rebase_target])
-for patch in patches.splitlines():
-  elem = patch.split(" ", 2)
-  # print "patch: " + patch
-  # print "elem[0]: '%s' elem[1]: '%s' elem[2]: '%s'" %
-  #        (elem[0], elem[1], elem[2])
-  if elem[0] == "+":
-    # patch not found upstream
-    sha = elem[1][:12]
-    # Try to find patch in stable branch. If it is there, drop it after all.
-    # If not, we may need to apply it.
-    c.execute("select sha, origin from stable where sha is '%s'" % sha)
-    found = c.fetchone()
-    if found:
-      c.execute("UPDATE commits SET disposition=('drop') where sha='%s'" % sha)
-      c.execute("UPDATE commits SET reason=('%s') where sha='%s'" %
-                (found[1], sha))
-    else:
-      c.execute("UPDATE commits SET disposition=('pick') where sha='%s'" % sha)
-      c.execute("UPDATE commits SET reason=('') where sha='%s'" % sha)
+  conn.commit()
 
-os.chdir(workdir)
-conn.commit()
-conn.close()
+  # "git cherry -v <target>" on branch rebase_baseline gives us a list
+  # of patches to apply.
+  patches = subprocess.check_output(['git', 'cherry', '-v', rebase_target])
+  for patch in patches.splitlines():
+    elem = patch.split(" ", 2)
+    # print "patch: " + patch
+    # print "elem[0]: '%s' elem[1]: '%s' elem[2]: '%s'" %
+    #        (elem[0], elem[1], elem[2])
+    if elem[0] == "+":
+      # patch not found upstream
+      sha = elem[1][:12]
+      # Try to find patch in stable branch. If it is there, drop it after all.
+      # If not, we may need to apply it.
+      c.execute("select sha, origin from stable where sha is '%s'" % sha)
+      found = c.fetchone()
+      if found:
+        c.execute("UPDATE commits SET disposition=('drop') where sha='%s'" % sha)
+        c.execute("UPDATE commits SET reason=('%s') where sha='%s'" %
+                  (found[1], sha))
+        c.execute("UPDATE commits SET updated=('%d') where sha='%s'" % (NOW(), sha))
+      else:
+        # We need to check if the commit is already marked as drop
+        # with a reason other than "default". If so, don't update it.
+        c.execute("select disposition, reason from commits where sha='%s'" % sha)
+        found = c.fetchone()
+        if found and found[0] == "drop" and found[1] == "default":
+          c.execute("UPDATE commits SET disposition=('pick') where sha='%s'" % sha)
+          c.execute("UPDATE commits SET reason=('') where sha='%s'" % sha)
+          c.execute("UPDATE commits SET updated=('%d') where sha='%s'" % (NOW(), sha))
+
+  os.chdir(workdir)
+  conn.commit()
+  conn.close()
+
+if not os.path.isfile(rebasedb):
+  createdb()
+
+update_stable(stable_path, stable_commits, "stable")
+update_stable(android_path, baseline_commits, "android")
+update_commits()
