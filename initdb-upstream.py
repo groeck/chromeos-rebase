09a6e1552185069f6a944b2d7328dddf1b0d85f9
Guenter Roeck <groeck@chromium.org>
2019-07-12 13:34:00 -0700

    initdb-upstream: Identify "Fixes:" and store in database
    Identify "Fixes:" tags and store both fixed and fixing SHA
    in the database.
    
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- initdb-upstream.py
+++ initdb-upstream.py
@@ -1,5 +1,6 @@
-import sqlite3
 import os
+import re
+import sqlite3
 import subprocess
 from config import upstreamdb, upstream_path, rebase_baseline, rebase_target
 
@@ -13,6 +14,8 @@ try:
 except:
     pass
 
+rf = re.compile('^Fixes: ([a-zA-Z0-9]+).*')
+
 conn = sqlite3.connect(upstreamdb)
 
 c = conn.cursor()
@@ -25,37 +28,48 @@ c.execute("CREATE TABLE files (sha text, filename text)")
 c.execute("CREATE INDEX file_sha ON files (sha)")
 c.execute("CREATE INDEX file_name ON files (filename)")
 
+c.execute("CREATE TABLE fixes (sha text, fsha text)")
+c.execute("CREATE INDEX sha ON fixes (sha)")
+
 conn.commit()
 
 os.chdir(upstream_path)
 
-commits = subprocess.check_output(['git', 'log', '--abbrev=12', '--oneline', '--no-merges', upstream_drop])
-for commit in commits.splitlines():
-    if commit != "":
+def handle(range, baseline):
+    commits = subprocess.check_output(['git', 'log', '--abbrev=12', '--oneline', '--no-merges', range])
+    for commit in commits.splitlines():
+      if commit != "":
         elem = commit.split(" ", 1)
-        sha = elem[0]
-        description = elem[1].rstrip('\n')
+	sha = elem[0]
+	description = elem[1].rstrip('\n')
 	description = description.decode('latin-1') if isinstance(description, str) else description
-	c.execute("INSERT INTO commits(sha, description, in_baseline) VALUES (?, ?, ?)", (sha, description,1,))
+	c.execute("INSERT INTO commits(sha, description, in_baseline) VALUES (?, ?, ?)",
+		  (sha, description, baseline,))
 	filenames = subprocess.check_output(['git', 'show', '--name-only', '--format=', sha])
 	for fn in filenames.splitlines():
 	    if fn != "":
-		c.execute("INSERT INTO files(sha, filename) VALUES (?, ?)", (sha,fn))
+		c.execute("INSERT INTO files(sha, filename) VALUES (?, ?)", (sha, fn))
+	# Now check if this patch fixes a previous patch.
+	description = subprocess.check_output(['git', 'show', '-s', '--pretty=format:%b', sha])
+	m = rf.search(description)
+	if m and m.group(1):
+	    try:
+	        # Normalize fsha to 12 characters.
+		cmd = 'git show -s --pretty=format:%%H %s 2>/dev/null' % m.group(1)
+		fsha = subprocess.check_output(cmd, shell=True)
+		print "Commit %s has been fixed by %s" % (fsha[0:12], sha[0:12])
+		# Insert in reverse order: sha is fixed by fsha
+		c.execute("INSERT into fixes (sha, fsha) VALUES (?, ?)", (fsha[0:12], sha[0:12]))
+	    except:
+		print "Skipping '%s': Not found" % m.group(0)
+		# The Fixes: tag may be wrong. The sha may not be in the
+		# upstream kernel, or the format may be completely wrong and
+		# m.group(1) may not be a sha in the first place.
+		# In that case, do nothing.
+		pass
+    conn.commit()
 
-conn.commit()
+handle(upstream_drop, 1)
+handle(upstream_pick, 0)
 
-commits = subprocess.check_output(['git', 'log', '--abbrev=12', '--oneline', '--no-merges', upstream_pick])
-for commit in commits.splitlines():
-    if commit != "":
-        elem = commit.split(" ", 1)
-        sha = elem[0]
-        description = elem[1].rstrip('\n')
-	description = description.decode('latin-1') if isinstance(description, str) else description
-	c.execute("INSERT INTO commits(sha, description, in_baseline) VALUES (?, ?, ?)", (sha, description,0,))
-	filenames = subprocess.check_output(['git', 'show', '--name-only', '--format=', sha])
-	for fn in filenames.splitlines():
-	    if fn != "":
-		c.execute("INSERT INTO files(sha, filename) VALUES (?, ?)", (sha,fn))
-
-conn.commit()
 conn.close()
