b22c5a8f4e3d385e9a54738f7bd618cc5ad591ae
Guenter Roeck <groeck@chromium.org>
2019-08-19 13:30:28 -0700

    initdb-upstream: Improve fixup detection
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- initdb-upstream.py
+++ initdb-upstream.py
@@ -15,10 +15,12 @@ except:
     pass
 
 rf = re.compile('^\s*Fixes: ([a-zA-Z0-9]+).*')
+rdesc = re.compile('.* \("([^"]+)"\).*')
 
 conn = sqlite3.connect(upstreamdb)
 
 c = conn.cursor()
+c2 = conn.cursor()
 
 # Create tables
 c.execute("CREATE TABLE commits (sha text, description text, in_baseline integer)")
@@ -36,38 +38,50 @@ conn.commit()
 os.chdir(upstream_path)
 
 def handle(range, baseline):
-    commits = subprocess.check_output(['git', 'log', '--abbrev=12', '--oneline', '--no-merges', range])
+    commits = subprocess.check_output(['git', 'log', '--abbrev=12', '--oneline', '--no-merges', '--reverse', range])
     for commit in commits.splitlines():
       if commit != "":
         elem = commit.split(" ", 1)
-	sha = elem[0]
-	description = elem[1].rstrip('\n')
-	description = description.decode('latin-1') if isinstance(description, str) else description
-	c.execute("INSERT INTO commits(sha, description, in_baseline) VALUES (?, ?, ?)",
-		  (sha, description, baseline,))
-	filenames = subprocess.check_output(['git', 'show', '--name-only', '--format=', sha])
-	for fn in filenames.splitlines():
-	    if fn != "":
-		c.execute("INSERT INTO files(sha, filename) VALUES (?, ?)", (sha, fn))
-	# Now check if this patch fixes a previous patch.
-	description = subprocess.check_output(['git', 'show', '-s', '--pretty=format:%b', sha])
-	for d in description.splitlines():
-	  m = rf.search(d)
-	  if m and m.group(1):
-	    try:
-	        # Normalize fsha to 12 characters.
-		cmd = 'git show -s --pretty=format:%%H %s 2>/dev/null' % m.group(1)
-		fsha = subprocess.check_output(cmd, shell=True)
-		print "Commit %s has been fixed by %s" % (fsha[0:12], sha[0:12])
-		# Insert in reverse order: sha is fixed by fsha
-		c.execute("INSERT into fixes (sha, fsha) VALUES (?, ?)", (fsha[0:12], sha[0:12]))
-	    except:
-		print "Skipping '%s' for %s: Not found" % (m.group(0), sha)
-		# The Fixes: tag may be wrong. The sha may not be in the
-		# upstream kernel, or the format may be completely wrong and
-		# m.group(1) may not be a sha in the first place.
-		# In that case, do nothing.
-		pass
+        sha = elem[0]
+        description = elem[1].rstrip('\n')
+        description = description.decode('latin-1') if isinstance(description, str) else description
+        c.execute("INSERT INTO commits(sha, description, in_baseline) VALUES (?, ?, ?)",
+                  (sha, description, baseline,))
+        filenames = subprocess.check_output(['git', 'show', '--name-only', '--format=', sha])
+        for fn in filenames.splitlines():
+          if fn != "":
+            c.execute("INSERT INTO files(sha, filename) VALUES (?, ?)", (sha, fn))
+        # Now check if this patch fixes a previous patch.
+        description = subprocess.check_output(['git', 'show', '-s', '--pretty=format:%b', sha])
+        for d in description.splitlines():
+          m = rf.search(d)
+          fsha=None
+          if m and m.group(1):
+            try:
+              # Normalize fsha to 12 characters.
+              cmd = 'git show -s --pretty=format:%%H %s 2>/dev/null' % m.group(1)
+              fsha = subprocess.check_output(cmd, shell=True)
+            except:
+              print "Commit '%s' for SHA '%s': Not found" % (m.group(0), sha)
+              m=rdesc.search(d)
+              if m:
+                desc=m.group(1)
+                desc = desc.replace("'", "''")
+                c2.execute("select sha from commits where description is '%s'" % desc)
+                fsha = c2.fetchone()
+                if fsha:
+                  fsha=fsha[0]
+                  print "  Real SHA is '%s'" % fsha
+              # The Fixes: tag may be wrong. The sha may not be in the
+              # upstream kernel, or the format may be completely wrong and
+              # m.group(1) may not be a sha in the first place.
+              # In that case, do nothing.
+              pass
+          if fsha:
+            print "Commit %s has been fixed by %s" % (fsha[0:12], sha)
+            # Insert in reverse order: sha is fixed by fsha
+            c.execute("INSERT into fixes (sha, fsha) VALUES (?, ?)", (fsha[0:12], sha))
+
     conn.commit()
 
 handle(upstream_drop, 1)
