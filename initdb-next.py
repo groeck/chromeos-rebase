2520ee64d44737ac095675587df1ef0da0ac01f6
Guenter Roeck <groeck@chromium.org>
2019-10-01 09:41:46 -0700

    initdb-next: Use common.py
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- initdb-next.py
+++ initdb-next.py
@@ -6,49 +6,48 @@ from __future__ import print_function
 import sqlite3
 import os
 import subprocess
-from config import nextdb, next_path, rebase_target
+from config import next_path, rebase_target
+from common import workdir, nextdb, createdb
 
-next_pick = rebase_target + '..'
+def mktables(c):
+  # Create tables
+  c.execute("CREATE TABLE commits (sha text, subject text, in_baseline integer)")
+  c.execute("CREATE UNIQUE INDEX commit_sha ON commits (sha)")
 
-workdir = os.getcwd()
+  c.execute("CREATE TABLE files (sha text, filename text)")
+  c.execute("CREATE INDEX file_sha ON files (sha)")
+  c.execute("CREATE INDEX file_name ON files (filename)")
 
-try:
-  os.remove(nextdb)
-except OSError:
-  pass
+def handle(c):
+  next_pick = rebase_target + '..'
+  commits = subprocess.check_output(['git', 'log', '--oneline', '--no-merges',
+                                    next_pick])
+  for commit in commits.splitlines():
+    if commit != "":
+      elem = commit.split(" ", 1)
+      sha = elem[0]
+      subject = elem[1].rstrip('\n')
+      subject = subject.decode('latin-1') \
+          if isinstance(subject, str) else subject
+      c.execute("INSERT INTO commits(sha, subject, in_baseline) VALUES (?, ?, ?)",
+                (sha, subject, 0,))
+      filenames = subprocess.check_output(['git', 'show', '--name-only',
+                                           '--format=', sha])
+      for fn in filenames.splitlines():
+        if fn != "":
+          c.execute("INSERT INTO files(sha, filename) VALUES (?, ?)", (sha, fn))
 
-conn = sqlite3.connect(nextdb)
+def initdb_next():
+  # Always re-create the 'next' database.
+  # Its SHAs are unstable and thus can not be relied on.
+  createdb(nextdb, mktables)
+  os.chdir(next_path)
+  conn = sqlite3.connect(nextdb)
+  c = conn.cursor()
+  handle(c)
+  conn.commit()
+  conn.close()
+  os.chdir(workdir)
 
-c = conn.cursor()
-
-# Create tables
-c.execute("CREATE TABLE commits (sha text, subject text, in_baseline integer)")
-c.execute("CREATE UNIQUE INDEX commit_sha ON commits (sha)")
-
-c.execute("CREATE TABLE files (sha text, filename text)")
-c.execute("CREATE INDEX file_sha ON files (sha)")
-c.execute("CREATE INDEX file_name ON files (filename)")
-
-conn.commit()
-
-os.chdir(next_path)
-
-commits = subprocess.check_output(['git', 'log', '--oneline', '--no-merges',
-                                   next_pick])
-for commit in commits.splitlines():
-  if commit != "":
-    elem = commit.split(" ", 1)
-    sha = elem[0]
-    subject = elem[1].rstrip('\n')
-    subject = subject.decode('latin-1') \
-        if isinstance(subject, str) else subject
-    c.execute("INSERT INTO commits(sha, subject, in_baseline) VALUES (?, ?, ?)",
-              (sha, subject, 0,))
-    filenames = subprocess.check_output(['git', 'show', '--name-only',
-                                         '--format=', sha])
-    for fn in filenames.splitlines():
-      if fn != "":
-        c.execute("INSERT INTO files(sha, filename) VALUES (?, ?)", (sha, fn))
-
-conn.commit()
-conn.close()
+if __name__ == "__main__":
+  initdb_next()
