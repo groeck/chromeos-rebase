749113118cfd1bce9cd6b057024e40c67eeb8c9c
Guenter Roeck <groeck@chromium.org>
2021-04-26 06:53:41 -0700

    run-scripts: Always reinstall google_auth_oauthlib
    google_auth_oauthlib tendss to magically uninstall itself.
    Always try to reinstall when running scripts to avoid
    unnecessary failures.
    
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- run-scripts.sh
+++ run-scripts.sh
@@ -3,6 +3,9 @@
 progdir="$(dirname $0)"
 logfile="/tmp/run-scripts.$(date +%F).log"
 
+# IT loves uninstalling stuff, so just force a reinstall
+pip3 install google_auth_oauthlib >/dev/null 2>&1
+
 cd "${progdir}"
 ./setup.sh > "${logfile}" 2>&1
 ./genspreadsheet.py >> "${logfile}" 2>&1
