395195e8aaab231ce2d96f0a8664e7d8797cb2ae
Guenter Roeck <groeck@google.com>
2021-04-27 16:16:20 -0700

    initdb-drop: Reformat
    Run "pyformat -s 4 -i --force_quote_type=single".
    No functional change.
    
    Signed-off-by: Guenter Roeck <groeck@google.com>

--- initdb-drop.py
+++ initdb-drop.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 import subprocess
 
-dbname = "drop49.db"
+dbname = 'drop49.db'
 
 try:
     os.remove(dbname)
@@ -14,8 +14,9 @@ conn = sqlite3.connect(dbname)
 c = conn.cursor()
 
 # Create tables
-c.execute("CREATE TABLE droplist (sha text, resolution text, reason text, dsha text)")
-c.execute("CREATE UNIQUE INDEX drop_sha ON droplist (sha)")
+c.execute(
+    'CREATE TABLE droplist (sha text, resolution text, reason text, dsha text)')
+c.execute('CREATE UNIQUE INDEX drop_sha ON droplist (sha)')
 
 conn.commit()
 conn.close()
