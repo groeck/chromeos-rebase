c2559cefb62e62656d61e244d762f580acc2fdf8
Guenter Roeck <groeck@google.com>
2021-04-27 16:17:50 -0700

    revertlist: Reformat
    Run "pyformat -s 4 -i --force_quote_type=single".
    No functional change.
    
    Signed-off-by: Guenter Roeck <groeck@google.com>

--- revertlist.py
+++ revertlist.py
@@ -7,8 +7,10 @@ import re
 import time
 from config import rebasedb
 
+
 def NOW():
-  return int(time.time())
+    return int(time.time())
+
 
 workdir = os.getcwd()
 
@@ -20,19 +22,24 @@ c2 = conn.cursor()
 
 rp = re.compile('Revert "(.*)"')
 
+
 def mark_disposition(db, sha, disposition, reason, revert_sha):
-    '''Mark sha for given disposition. If available, add revert_sha into dsha.'''
+    """Mark sha for given disposition.
+
+    If available, add revert_sha into dsha.
+    """
 
     c = db.cursor()
 
-    print("    Marking %s for %s (%s)" % (sha, disposition, reason))
+    print('    Marking %s for %s (%s)' % (sha, disposition, reason))
 
-    cmd='UPDATE commits SET disposition="%s", reason="%s", updated="%d"' % (disposition, reason, NOW())
+    cmd = 'UPDATE commits SET disposition="%s", reason="%s", updated="%d"' % (
+        disposition, reason, NOW())
     if revert_sha:
         cmd += ', dsha="%s"' % revert_sha
     cmd += ' where sha="%s"' % sha
 
-    print("  SQL: %s" % cmd)
+    print('  SQL: %s' % cmd)
 
     c.execute(cmd)
 
@@ -49,42 +56,51 @@ def mark_disposition(db, sha, disposition, reason, revert_sha):
 # c.execute("INSERT INTO files VALUES (sha,filename)")
 # sha and filename must be in ' '
 
-c.execute("select committed, sha, disposition, subject from commits")
+c.execute('select committed, sha, disposition, subject from commits')
 for (committed, sha, disposition, desc) in c.fetchall():
-  # If the patch has already been dropped, don't bother any further
-  if disposition == "drop":
-    continue
-  m = rp.search(desc)
-  if m:
-    ndesc = m.group(1)
-    print("Found revert : '%s' (%s)" % (desc.replace("'", "''"), sha))
-    ndesc = ndesc.replace("'", "''")
-    c2.execute("select committed, sha from commits where subject is '%s'"
-               % ndesc)
-    # Search for commit closest to the revert in the past
-    # (There may be multiple if the revert was repeated)
-    revert_committed = None
-    revert_sha = None
-    for (_committed, _sha,) in c2.fetchall():
-      if _committed < committed:
-        if not revert_sha or revert_committed < _committed:
-          revert_committed = _committed
-          revert_sha = _sha
+    # If the patch has already been dropped, don't bother any further
+    if disposition == 'drop':
+        continue
+    m = rp.search(desc)
+    if m:
+        ndesc = m.group(1)
+        print("Found revert : '%s' (%s)" % (desc.replace("'", "''"), sha))
+        ndesc = ndesc.replace("'", "''")
+        c2.execute("select committed, sha from commits where subject is '%s'" %
+                   ndesc)
+        # Search for commit closest to the revert in the past
+        # (There may be multiple if the revert was repeated)
+        revert_committed = None
+        revert_sha = None
+        for (
+                _committed,
+                _sha,
+        ) in c2.fetchall():
+            if _committed < committed:
+                if not revert_sha or revert_committed < _committed:
+                    revert_committed = _committed
+                    revert_sha = _sha
 
-    # <sha> is the revert of <revert_sha>. Mark both as reverted.
-    if revert_sha:
-      mark_disposition(conn, revert_sha, 'drop', 'reverted', sha)
-      mark_disposition(conn, sha, 'drop', 'reverted', revert_sha)
-      # Now check if we can find a FIXUP: of <revert_sha>. It must
-      # have been committed between <revert_sha> and <sha>.
-      fdesc = "FIXUP: %s" % ndesc
-      c2.execute("select committed, sha from commits where subject is '%s'" % fdesc)
-      for (_committed, _sha,) in c2.fetchall():
-          if _committed <= committed and _committed >= revert_committed:
-              mark_disposition(conn, _sha, 'drop', "fixup/reverted", revert_sha)
-    else:
-      mark_disposition(conn, sha, 'pick', 'revisit', None)
-      print("    No matching commit found")
+        # <sha> is the revert of <revert_sha>. Mark both as reverted.
+        if revert_sha:
+            mark_disposition(conn, revert_sha, 'drop', 'reverted', sha)
+            mark_disposition(conn, sha, 'drop', 'reverted', revert_sha)
+            # Now check if we can find a FIXUP: of <revert_sha>. It must
+            # have been committed between <revert_sha> and <sha>.
+            fdesc = 'FIXUP: %s' % ndesc
+            c2.execute(
+                "select committed, sha from commits where subject is '%s'" %
+                fdesc)
+            for (
+                    _committed,
+                    _sha,
+            ) in c2.fetchall():
+                if _committed <= committed and _committed >= revert_committed:
+                    mark_disposition(conn, _sha, 'drop', 'fixup/reverted',
+                                     revert_sha)
+        else:
+            mark_disposition(conn, sha, 'pick', 'revisit', None)
+            print('    No matching commit found')
 
 conn.commit()
 
