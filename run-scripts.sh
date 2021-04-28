83d5a6154342b50654d964df3e572e81f9745d20
Guenter Roeck <groeck@google.com>
2021-04-28 15:25:57 -0700

    run-scripts: Fix issues reported by shellcheck
    Signed-off-by: Guenter Roeck <groeck@google.com>

--- run-scripts.sh
+++ run-scripts.sh
@@ -2,13 +2,13 @@
 
 baseline="$(python3 -c 'from config import rebase_baseline_branch; print(rebase_baseline_branch)')"
 
-progdir="$(dirname $0)"
+progdir=$(dirname "$0")
 logfile="/tmp/run-scripts.${baseline}.$(date +%F).log"
 
 # IT loves uninstalling stuff, so just force a reinstall
 pip3 install google_auth_oauthlib python-Levenshtein fuzzywuzzy >/dev/null 2>&1
 
-cd "${progdir}"
+cd "${progdir}" || exit 1
 ./setup.sh > "${logfile}" 2>&1
 ./genspreadsheet.py >> "${logfile}" 2>&1
 ./genstats.py >> "${logfile}" 2>&1
