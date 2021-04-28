efcb3d5638fc34e0e553c358d7900f829695c61e
Guenter Roeck <groeck@google.com>
2021-04-28 15:24:32 -0700

    add-drop: Fix issues reported by shellcheck
    Signed-off-by: Guenter Roeck <groeck@google.com>

--- add-drop.sh
+++ add-drop.sh
@@ -7,4 +7,4 @@ reason=$3
 # TODO: Check if sha is in database, and if parameters are correct
 
 sqlite3 drop49.db "insert into droplist (sha, resolution, reason)
-			values ('${sha}', '${resolution}', '${reason}');"
+                   values ('${sha}', '${resolution}', '${reason}');"
